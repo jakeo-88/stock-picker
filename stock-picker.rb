6698f2b19c3e2020306b161dcec89d9175e91601
Jake <jakegreen2016@gmail.com>
2020-10-27 21:29:37 -0800

    create ruby file with pseudo-code

---
+++ stock-picker.rb
@@ -0,0 +1,11 @@
+#make variable
+
+#create method
+
+    #create loop that goes through Array to find the greatest difference 
+
+        #left value must be less than the right value
+    
+        #case for first value being the highest 
+
+    #return pair of lowest and highest days with the greatest difference
